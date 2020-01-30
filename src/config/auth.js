9790a23502fa55c1fd9c6faf2663bd56f360e258
Nouani <nouanigabriel@hotmail.com>
2020-01-30 17:05:36 -0300

    Migration(create-users), Seed(admin-user), Model(user) e SessionController

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+    secret: 'c8d6e2d7e53ccbfbd0d96868aa16199a',
+    expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 
+import SessionController from './app/controllers/SessionController';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello World' }));
+routes.post('/sessions', SessionController.store);
 
 export default routes;
