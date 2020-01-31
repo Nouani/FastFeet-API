b6ba691377309a53850985f4c9d405f07549d993
Nouani <nouanigabriel@hotmail.com>
2020-01-30 22:24:06 -0300

    Atualização do destinatário

--- src/app/controllers/RecipientController.js
+++ src/app/controllers/RecipientController.js
@@ -30,6 +30,32 @@ class RecipientController {
 
         return res.json({ recipient });
     }
+
+    async update(req, res) {
+        const schema = Yup.object().shape({
+            destinatary_name: Yup.string().required(),
+            street: Yup.string().required(),
+            number: Yup.number().required(),
+            complement: Yup.string(),
+            state: Yup.string().required(),
+            city: Yup.string().required(),
+            zip_code: Yup.number().required(),
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Validation fails' });
+        }
+
+        let recipient = await Recipient.findByPk(req.params.id);
+
+        if (!recipient) {
+            return res.status(400).json({ error: 'Recipient does not found' });
+        }
+
+        recipient = await recipient.update(req.body);
+
+        return res.json(recipient);
+    }
 }
 
 export default new RecipientController();
