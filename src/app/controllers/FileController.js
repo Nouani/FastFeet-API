9db28df7f71c0d79fc8a91298e1029bad9746a38
Nouani <nouanigabriel@hotmail.com>
2020-02-04 00:41:17 -0300

    Configuração do multer

---
+++ src/app/controllers/FileController.js
@@ -0,0 +1,7 @@
+class FileController {
+    async store(req, res) {
+        return res.json();
+    }
+}
+
+export default new FileController();
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,16 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
 import DeliverymanController from './app/controllers/DeliverymanController';
 
 import authMiddleware from './app/middlewares/auth';
+import multerConfig from './config/multer';
+import FileController from './app/controllers/FileController';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/sessions', SessionController.store);
 
@@ -19,5 +23,7 @@ routes.get('/deliverymen', DeliverymanController.index);
 routes.post('/deliverymen', DeliverymanController.store);
 routes.put('/deliverymen/:id', DeliverymanController.update);
 routes.delete('/deliverymen/:id', DeliverymanController.destroy);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
